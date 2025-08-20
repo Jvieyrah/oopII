7b96eec7408766a8d3a9621eacec8a2cf2c010d4
Alex Araujo <alexsaspbr@gmail.com>
2025-08-20 20:45:42 -0300

    finalizando conteudo interfaces e exercicios

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,11 @@
+import exemplo.ecommerce.Produto;
 import exemplo.imposto.ICMS;
 import exemplo.imposto.IOF;
 import exemplo.imposto.Imposto;
 import exemplo.imposto.ImpostoRecord;
 
-import java.util.List;
+import java.math.BigDecimal;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
@@ -24,6 +26,24 @@ public class Main {
 
         ImpostoRecord iva = new ImpostoRecord("2", "IVA");
         //iva.taxa();
+
+        Comparator<Produto> compImposto = Comparator.comparing(Produto::getId)
+                                                    .thenComparing(Produto::getValor);
+        //compImposto.compare(new Produto(), new Produto());
+
+        Integer numero = Integer.valueOf(10);
+        System.out.printf("O numeros sao iguais %d", numero.compareTo(Integer.valueOf(15)));
+
+
+
+
+
+        List<Produto> produtos = new ArrayList<>();
+        produtos.add(new Produto(2,  "TV 55", new BigDecimal(4500)));
+        produtos.add(new Produto(1,  "PS 5", new BigDecimal(3800)));
+        Collections.sort(produtos);
+        Collections.sort(produtos, Comparator.comparing(Produto::getValor));
+        produtos.sort((p1, p2) -> p1.getId().compareTo(p2.getId()));
 
 
     }
